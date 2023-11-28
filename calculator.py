19231147d612446c915c527cf05306dd780032a8
OpusLearning <james@opuslearning.co.uk>
2023-11-28 17:03:54 -0500

    Add advanced features to calculator

--- calculator.py
+++ calculator.py
@@ -10,13 +10,22 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+def power(x, y):
+    return x ** y
+
+def square_root(x):
+    return x ** 0.5
+
+
 print("Select operation:")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
+print("5. Power")
+print("6. Square Root")
 
-choice = input("Enter choice(1/2/3/4): ")
+choice = input("Enter choice(1/2/3/4/5/6): ")
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
@@ -29,6 +38,11 @@ elif choice == '3':
     print(num1, "*", num2, "=", multiply(num1, num2))
 elif choice == '4':
     print(num1, "/", num2, "=", divide(num1, num2))
+elif choice == "5":
+    print(num1, "**", num2, "=", power(num1,num2))
+elif choice == "6":
+    print(num1, "root", num2, "=", square_root(num1,num2))
+
 else:
     print("Invalid Input")
 
